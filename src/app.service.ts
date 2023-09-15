d07bb4f2e88d659a8d7a614179da6080ba2e7cf9
Ronny da Silva <58692160+ronny15652@users.noreply.github.com>
2023-09-15 17:34:09 -0300

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '/transaction  -  para o CRUD de Transações <br/>/category   -  para o CRUD de Categorias ';
   }
 }
